a7abdc0416371541b0a62081802cf340a1ba1f49
Sydney Barlow <sydneybarlow44@gmail.com>
2018-07-24 23:41:40 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,7 +10,7 @@ class SchoolClassesController < ApplicationController
   end
   
   def create
-    binding.pry
+    # binding.pry
     @school_class = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
     redirect_to school_class_path(@school_class)
   end
