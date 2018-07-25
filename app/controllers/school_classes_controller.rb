15f8a519c13fa0cdcd0d63ede196a0f9b123c330
Sydney Barlow <sydneybarlow44@gmail.com>
2018-07-25 00:16:10 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -25,7 +25,7 @@ class SchoolClassesController < ApplicationController
   
   def update 
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params.require(:student_class))
+    @school_class.update(params.require(:school_class))
     redirect_to school_class_path(@school_class)
   end
   
