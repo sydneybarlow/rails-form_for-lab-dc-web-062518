22c877c29c28d6a8fa15ae8469aab4b51da32d56
Sydney Barlow <sydneybarlow44@gmail.com>
2018-07-25 00:19:03 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class SchoolClassesController < ApplicationController
+  
+  def index
+    @school_classes = SchoolClass.all
+  end
+  
+  def new 
+    @school_class = SchoolClass.new 
+  end
+  
+  def create
+    @school_class = SchoolClass.create(params[:school_class])
+    redirect_to school_class_path(@school_class)
+  end
+  
+  def show 
+    # binding.pry
+    @school_class = SchoolClass.find(params[:id])
+  end
+  
+  def edit 
+    @school_class = SchoolClass.find(params[:id])
+  end
+  
+  def update 
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
+  end
+  
+end  
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class StudentsController < ApplicationController
+  
+  def index
+    @students = Student.all
+  end
+  
+  def new
+    @student = Student.new
+  end
+  
+  def create 
+    # binding.pry
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
+  end
+  
+  def show 
+    # binding.pry
+    @student = Student.find(params[:id])
+  end
+  
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update 
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to student_path(@student)
+  end
+  
+end  
